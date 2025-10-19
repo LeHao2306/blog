94b0145820464ca9ee7995f2585c1d803136729a
LeHao2306 <lenhuthao23062004@gmail.com>
2025-10-19 06:29:42 +0000

    Update site URL and base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,8 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://LeHao2306.github.io/blog',
+	base: '/blog/',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
